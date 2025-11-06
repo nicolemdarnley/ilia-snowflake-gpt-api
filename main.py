c998f94d72e31c5f65c9bf3d75bb4b1e5cfc3308
nicolemdarnley <76493061+nicolemdarnley@users.noreply.github.com>
2025-11-06 17:34:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_gpt_data():
         )
 
         cs = ctx.cursor()
-        cs.execute("SELECT * FROM gpt_innovation_forecast_analyst")
+        cs.execute("SELECT * FROM gpt_innovation_forecast_analyst LIMIT {limit} OFFSET {offset}")
         columns = [col[0] for col in cs.description]
         rows = cs.fetchall()
         cs.close()
